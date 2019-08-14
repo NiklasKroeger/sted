df40d7eeb1a51361fe46089d061fae671b8f8d0a
Niklas Kröger <kroeger.niklas@gmail.com>
2019-08-14 22:00:56 +0200

    Only show welcome message if no file is given

--- sted.c
+++ sted.c
@@ -216,7 +216,7 @@ void editorDrawRows(struct abuf *ab) {
     int y;
     for (y = 0; y < E.screenrows; y++) {
         if (y >= E.numrows) {
-            if (y == E.screenrows / 3) {
+            if (E.numrows == 0 && y == E.screenrows / 3) {
                 char welcome[80];
                 int welcomelen = snprintf(welcome, sizeof(welcome),
                         "sted - simple text editor -- version %s", STED_VERSION);
