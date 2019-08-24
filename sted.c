189143cd2ff816e493d0975f46d12e0e3a603aad
Niklas Kröger <kroeger.niklas@gmail.com>
2019-08-24 20:29:42 +0200

    Add newline when `Enter` key is pressed

--- sted.c
+++ sted.c
@@ -573,7 +573,7 @@ void editorProcessKeypress() {
 
     switch (c) {
         case '\r':
-            /* TODO */
+            editorInsertNewline();
             break;
 
         case CTRL_KEY('q'):
