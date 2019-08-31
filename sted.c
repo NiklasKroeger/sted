6351f2629b0ff4a9869d4596c4b748c5a068b470
Niklas Kröger <kroeger.niklas@gmail.com>
2019-08-31 16:47:05 +0200

    Adjust calls to editorPrompt

--- sted.c
+++ sted.c
@@ -376,7 +376,7 @@ void editorOpen(char *filename) {
 
 void editorSave() {
     if (E.filename == NULL) {
-        E.filename = editorPrompt("Save as: %s (ESC to abort)");
+        E.filename = editorPrompt("Save as: %s (ESC to abort)", NULL);
         if (E.filename == NULL) {
             editorSetStatusMessage("Save aborted");
             return;
@@ -407,7 +407,7 @@ void editorSave() {
 /*** find ***/
 
 void editorFind() {
-    char *query = editorPrompt("Search: %s (ESC to cancel)");
+    char *query = editorPrompt("Search: %s (ESC to cancel)", NULL);
     if (query == NULL) return;
 
     int i;
